84d77f6bfc8760566079655731b79357e5e85e79
Junteng Jia <juntengjia@hotmail.com>
2018-12-16 14:12:41 -0500

    save

--- ts.py
+++ ts.py
@@ -5,3 +5,6 @@ from matplotlib.pylab import rcParams
 
 rcParams["figure.figsize"] = (15,6)
 data = pd.read_csv("AirPassengers.csv", index_col="Month", parse_dates=["Month"], date_parser=lambda date : pd.datetime.strptime(date, "%Y-%m"))
+ts = data["#Passengers"]
+rolling_mean = ts.rolling(3).mean()
+rolling_std = ts.rolling(3).std()
